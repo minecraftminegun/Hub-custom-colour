7740d442cdea311d3bae1f3fc869390972d7451c
minecraft_minegun <159155336+minecraftminegun@users.noreply.github.com>
2025-01-22 23:43:43 +0100

    Update Root2.lua

--- Root2.lua
+++ Root2.lua
@@ -352,7 +352,7 @@ function Kavo.CreateLib(kavName, themeList)
 
 local Player = game:GetService("Players").LocalPlayer
 local Mouse = Player:GetMouse()
-local Label = Instance.new("ScreenGui")
+local Label = Instance.new("Frame")
 local keybind = "k"
 
 Mouse.KeyDown:Connect(function(key)
